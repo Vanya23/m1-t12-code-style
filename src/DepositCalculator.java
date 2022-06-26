dab26c2deb9cb96cecf6056fbeee198d352d6104
ivan234 <ivpetrov87@yandex.ru>
2022-06-26 16:42:24 +0300

    После проверки

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -23,18 +23,21 @@ public class DepositCalculator {
         System.out.println("Введите сумму вклада в рублях:") ;
         int amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:") ;
-        period = scanner.nextInt( );
+        period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
         double out = 0;
         if (action == 1) out = calculateSimplePercent(amount, 0.06, period);
         else if (action == 2) {
-            out = calculateComplexPercent(amount, 0.06, period); }
-            System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
+            out = calculateComplexPercent(amount, 0.06, period);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
     }
 
     public static void main(String[] args) {
         new DepositCalculator().printMenu();
+
     }
+
 
 }
